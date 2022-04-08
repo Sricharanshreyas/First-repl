57012c8ee1405ea7ac34b5323926b2978a9f424c
Sricha <>
2022-04-08 09:23:33 +0000

    Initial commit

---
+++ first-project @ new course.py
@@ -0,0 +1,26 @@
+print("my name is Sricharan")
+print("")
+print("Hakuna")
+
+#excercise
+
+age=float(int(input("What is your age?")))
+
+
+try:
+    age1=age+1
+
+
+
+except:
+    print("Warning: Age must be a number")
+
+
+else:
+    print("Your age is", age)
+    print("you will son be",age1)
+
+n=0
+while n<5:
+    print("Value of n:",n)
+    n=n+1
